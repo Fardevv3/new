af6740424f0b7fe617c8e921ffa78f82809b47cd
Your Name <fastbotyt@gmail.com>
2022-06-19 18:39:48 +0700

    first commit

---
+++ command/info/scriptbot.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "scriptbot",
+	alias: ["script", "sc", "scbot"],
+	category: "info",
+	isSpam: true,
+	async run({ msg, conn }, { q, map, args }) {
+	  text = `Script Bot : https://github.com/Rizky878/rzky-multidevice/\n`
+	  text += `Recode : https://github.com/ZeetsuShin/Bot-MD\n\n`
+          text += "Dont Forget to forks n star XD"
+	  await msg.reply("```" + text + "```");
+	},
+};
